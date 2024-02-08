0d9ddbb9ed2e6ce1de03ca5b50e3f35fb9be35b4
ZweYaung <159310742+ZweYaung@users.noreply.github.com>
2024-02-08 14:00:04 +0630

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yayyyyy hapi hapi hapi";
+  question.innerHTML = "Thank u pr";
   gif.src =
     "https://media1.giphy.com/media/iCVzZwwE6QNAV2tEE0/giphy.gif";
 });
