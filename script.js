d5d2352e7cc0dc85a4a7a94d33302459a7a2a3f1
ZweYaung <159310742+ZweYaung@users.noreply.github.com>
2024-02-08 23:18:13 +0630

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const noBtn = document.querySelector(".no-btn");
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "Yayyy hapi hapi hapi";
   gif.src =
-    "https://media1.giphy.com/media/iCVzZwwE6QNAV2tEE0/giphy.gif";
+    "https://scontent.frgn10-1.fna.fbcdn.net/v/t39.30808-6/307452344_1710379612669413_3714347934930556651_n.jpg?stp=dst-jpg_p843x403&_nc_cat=105&ccb=1-7&_nc_sid=dd5e9f&_nc_ohc=mOEgx5Mu-CEAX-_ID0e&_nc_ht=scontent.frgn10-1.fna&oh=00_AfBcQTDQjcL04VXPG6lHwjpTMaYGLU5LfMRQ0pfRmT4QJQ&oe=65CAB062";
 });
 
 noBtn.addEventListener("mouseover", () => {
